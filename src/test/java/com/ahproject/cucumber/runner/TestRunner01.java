9b377c9ada4680de5fd8c79b67b85fa61c72878a
Olivier Di Giorgio <olivier.digiorgio@referty.com>
2016-11-17 11:14:51 +0100

    Essai report

--- src/test/java/com/ahproject/cucumber/runner/TestRunner01.java
+++ src/test/java/com/ahproject/cucumber/runner/TestRunner01.java
@@ -6,13 +6,14 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:cucumberFeatures"
+		strict = false
+		,features = "classpath:cucumberFeatures"
 		,glue={"com.ahproject.cucumber.steps"}
 		,monochrome = true
 		,tags={"@ContactPage"}
 		//,plugin={"pretty","junit:c:/temp/cucumber.xml"}
-		,plugin={"pretty","html:target/Destination"} // <--- Format HTML dans répertoire Workspace\..\target\Destination
-		//,plugin={"pretty","json:target/Destination/cucumber.json"} // <--- Format JSON dans répertoire Workspace\..\target\Destination
+		//,plugin={"pretty","html:target/Destination"} // <--- Format HTML dans répertoire Workspace\..\target\Destination
+		,plugin={"pretty","json:target/Destination/cucumber.json"} // <--- Format JSON dans répertoire Workspace\..\target\Destination
 		)
 
 public class TestRunner01 {
